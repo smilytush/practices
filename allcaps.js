d2d750e8a12425bddc180c2aaaf53e31b8b6dd9c
smilytush <77962555+smilytush@users.noreply.github.com>
2021-10-19 18:52:14 +0530

    Add files via upload

---
+++ allcaps.js
@@ -0,0 +1,2 @@
+const allCaps = (arr) => arr.map ((item) => item.toLowerCase());
+console.log (allCaps(["cat","bat","fat", "mat"]));
---
+++ areaOfCirle.js
@@ -0,0 +1,10 @@
+
+
+
+
+const areaOfCircle = (r) => {
+    const pi = 3.14;
+    return pi * r * r;
+
+console.log(areaOfCirle(7));
+};
